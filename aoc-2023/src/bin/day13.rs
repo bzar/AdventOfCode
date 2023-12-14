7ffb338af2ce7851a113331551e9641dcdcb4972
Teemu Erkkola <teemu.erkkola@gofore.com>
2023-12-14 10:41:54 +0200

    generalize rectmap

--- aoc-2023/src/bin/day13.rs
+++ aoc-2023/src/bin/day13.rs
@@ -1,8 +1,9 @@
+use aoc_2023::RectMap;
 use itertools::Itertools;
 use nom::{
     branch::alt,
     character::complete as ncc,
-    combinator::{all_consuming, value},
+    combinator::{all_consuming, map, value},
     multi::{many1, separated_list1},
     sequence::terminated,
     Finish,
@@ -15,7 +16,7 @@ enum Tile {
     Ash,
     Rock,
 }
-type Map = Vec<Vec<Tile>>;
+type Map = RectMap<Tile>;
 type Model = Vec<Map>;
 
 #[derive(Debug, PartialEq)]
@@ -29,52 +30,38 @@ fn parse(input: &str) -> nom::IResult<&str, Model> {
             value(Tile::Ash, ncc::char('.')),
             value(Tile::Rock, ncc::char('#')),
         ));
-        many1(terminated(many1(parse_tile), ncc::line_ending))(input)
+        map(
+            many1(terminated(many1(parse_tile), ncc::line_ending)),
+            RectMap::new,
+        )(input)
     }
     all_consuming(separated_list1(ncc::line_ending, parse_map))(input)
 }
 
-fn rows<'a>(
-    map: &'a Map,
-) -> impl Iterator<Item = impl Iterator<Item = &'a Tile> + Clone + core::fmt::Debug>
-       + 'a
-       + DoubleEndedIterator
-       + ExactSizeIterator
-       + core::fmt::Debug {
-    map.iter().map(|row| row.iter())
-}
-fn columns<'a>(
-    map: &'a Map,
-) -> impl Iterator<Item = impl Iterator<Item = &'a Tile> + Clone + core::fmt::Debug>
-       + 'a
-       + DoubleEndedIterator
-       + ExactSizeIterator
-       + core::fmt::Debug {
-    // Assume each row has equal length
-    (0..map[0].len()).map(|x| map.iter().map(move |row| &row[x]))
-}
 fn find_reflections(map: &Map) -> impl Iterator<Item = Reflection> + core::fmt::Debug + '_ {
-    let vertical = rows(map)
+    let vertical = map
+        .rows()
         .tuple_windows()
         .enumerate()
         .filter_map(|(i, (a, b))| a.zip(b).all(|(a, b)| a == b).then_some(i))
         .filter(|i| {
-            rows(map)
+            map.rows()
                 .take(i + 1)
                 .rev()
-                .zip(rows(map).skip(i + 1))
+                .zip(map.rows().skip(i + 1))
                 .all(|(a, b)| a.zip(b).all(|(a, b)| a == b))
         })
         .map(|i| Reflection::Vertical(i + 1));
-    let horizontal = columns(map)
+    let horizontal = map
+        .columns()
         .tuple_windows()
         .enumerate()
         .filter_map(|(i, (a, b))| a.zip(b).all(|(a, b)| a == b).then_some(i))
         .filter(|i| {
-            columns(map)
+            map.columns()
                 .take(i + 1)
                 .rev()
-                .zip(columns(map).skip(i + 1))
+                .zip(map.columns().skip(i + 1))
                 .all(|(a, b)| a.zip(b).all(|(a, b)| a == b))
         })
         .map(|i| Reflection::Horizontal(i + 1));
@@ -88,10 +75,10 @@ fn find_reflection_with_smudge(map: &Map) -> Option<Reflection> {
             Tile::Rock => Tile::Ash,
         }
     }
-    let coords = (0..map[0].len()).cartesian_product(0..map.len());
+    let coords = (0..map.width()).cartesian_product(0..map.height());
     let smudged_reflections = coords.flat_map(|(x, y)| {
         let mut m = map.clone();
-        m[y][x] = flip(&m[y][x]);
+        m.modify((x, y), |t| flip(t)).unwrap();
         find_reflections(&m).collect::<Vec<_>>().into_iter()
     });
 
--- aoc-2023/src/lib.rs
+++ aoc-2023/src/lib.rs
@@ -1,14 +1,57 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct RectMap<T: Clone + core::fmt::Debug>(Vec<Vec<T>>);
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<T: Clone + core::fmt::Debug> RectMap<T> {
+    pub fn new(rows: Vec<Vec<T>>) -> Self {
+        let width = rows.first().map(|r| r.len()).unwrap_or(0);
+        assert!(rows.iter().skip(1).all(|r| r.len() == width));
+        Self(rows)
+    }
+    pub fn width(&self) -> usize {
+        self.0.first().map(|r| r.len()).unwrap_or(0)
+    }
+    pub fn height(&self) -> usize {
+        self.0.len()
+    }
+    pub fn rows<'a>(
+        &'a self,
+    ) -> impl Iterator<Item = impl Iterator<Item = &'a T> + Clone + core::fmt::Debug>
+           + 'a
+           + DoubleEndedIterator
+           + ExactSizeIterator
+           + core::fmt::Debug {
+        self.0.iter().map(|row| row.iter())
+    }
+    pub fn columns<'a>(
+        &'a self,
+    ) -> impl Iterator<Item = impl Iterator<Item = &'a T> + Clone + core::fmt::Debug>
+           + 'a
+           + DoubleEndedIterator
+           + ExactSizeIterator
+           + core::fmt::Debug {
+        // Assume each row has equal length
+        (0..self.0[0].len()).map(|x| self.0.iter().map(move |row| &row[x]))
+    }
+    pub fn cells<'a>(
+        &'a self,
+    ) -> impl Iterator<Item = ((usize, usize), &'a T)> + DoubleEndedIterator + core::fmt::Debug
+    {
+        self.0.iter().enumerate().flat_map(|(y, row)| {
+            row.iter()
+                .enumerate()
+                .map(move |(x, value)| ((x, y), value))
+        })
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn get(&self, (x, y): (usize, usize)) -> Option<&T> {
+        self.0.get(y)?.get(x)
+    }
+    pub fn get_mut(&mut self, (x, y): (usize, usize)) -> Option<&mut T> {
+        self.0.get_mut(y)?.get_mut(x)
+    }
+    pub fn modify(&mut self, (x, y): (usize, usize), f: impl Fn(&T) -> T) -> Option<&T> {
+        let value = self.0.get_mut(y)?.get_mut(x)?;
+        *value = f(value);
+        Some(value)
     }
 }
